57ea55d5b75a6648d63617f6955d9f728c131fd5
Pedro Lemos <pedrotlgl@gmail.com>
2026-01-13 15:28:08 -0300

    feat: implement POST /polls route with validation and bulk insert

--- src/routes/poll.routes.js
+++ src/routes/poll.routes.js
@@ -1,11 +1,53 @@
 import { Router } from "express";
+import pool from "../config/database.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    message: "Lista de Enquetes",
-  });
+// Rota para Criar Enquete (POST)
+router.post("/", async (req, res) => {
+  const { title, start_date, end_date, options } = req.body;
+
+  if (!title || !start_date || !end_date || !options) {
+    return res.status(400).json({
+      error: "Todos os campos são obrigatórios.",
+    });
+  }
+
+  if(!Array.isArray(options) || options.length < 3){
+    return res.status(400).json({
+      error: "A enquete deve ter no mínimo 3 opções de resposta."
+    });
+  }
+
+  try {
+    const [result] = await pool.execute(
+      "INSERT INTO polls (title, start_date, end_date) VALUES (?, ?, ?)",
+      [title, start_date, end_date]
+    );
+
+    const pollId = result.insertId;
+
+    const optionsData = options.map((optionText) => [pollId, optionText]);
+
+    const placeholders = optionsData.map(() => "(?, ?)").join(", ");
+
+    const sqlOptions = `INSERT INTO poll_options (poll_id, option_text) VALUES ${placeholders}`;
+
+    const flatParams = optionsData.flat();
+
+    await pool.execute(sqlOptions, flatParams);
+
+    res.status(200).json({
+      message: "Enquete criada com sucesso!",
+      pollId: pollId,
+    });
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "Erro ao criar enquete.",
+    });
+  }
 });
 
 export default router;
