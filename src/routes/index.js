f7fad3c4bf97e35ff9ae43fc8b625c0080648f1f
Pedro Lemos <pedrotlgl@gmail.com>
2026-01-13 13:44:32 -0300

    refactor: separate app and server setup

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import pollRoutes from "./poll.routes.js";
+
+const router = Router();
+
+router.get("/health", (req, res) => {
+  res.json({
+    status: "ok",
+  });
+});
+
+router.use("/polls", pollRoutes);
+
+export default router;
